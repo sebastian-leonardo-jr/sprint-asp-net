f96bb4fd8a6eab305e00ae85047a285ef81c925c
Sebastian Adamo <seb.adamo10@gmail.com>
2020-06-17 16:25:33 +0100

    Initialising

---
+++ Sprint-App/Data/SprintContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sprint.Models;
+
+namespace Sprint.Data
+{
+    public class SprintContext : DbContext
+    {
+        public SprintContext(DbContextOptions<SprintContext> options) : base(options)
+        {
+      
+        }
+
+        public DbSet<Board> Board { get; set; }
+    }
+}
---
+++ Sprint-App/Startup.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Sprint.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Sprint
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddDbContext<SprintContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("SprintContext")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Main}/{action=Dashboard}/{id?}");
+            });
+        }
+    }
+}
